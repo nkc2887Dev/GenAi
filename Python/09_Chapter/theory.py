2f6751b1e28ea8445aadc3a5564e0e4fa6a3d383
nkc2887dev <nkc2887dev@gmail.com>
2025-04-11 17:12:47 +0530

    Python Learning...

--- Python/09_Chapter/theory.py
+++ Python/09_Chapter/theory.py
@@ -1,1 +1,16 @@
 ## OBJECT ORIENTED PROGRAMMING
+
+class Employer:
+    def __init__(self, name):
+        self.name=name
+    def get_name(self):
+        print("Hello", self)
+
+    @staticmethod
+    def greet(name):
+        print(f"name: {name}")
+        return f"name: {name}"
+
+a = Employer("DEV")
+b = a.greet("def")
+print(b)
